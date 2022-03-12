629cbd1058e9256d5f72300626245c390b3d13cc
@dosya_node <dostonbekoktmov765@gmail.com>
2022-03-12 16:22:10 +0500

    bag fixed

--- action/auth.js
+++ action/auth.js
@@ -4,7 +4,6 @@ let baza = [2125760802, 1892939498]
 
 bot.start((msg, next) => {
     const chat_id = msg.chat.id
-    // console.log(chat_id)
     if (!baza.includes(msg.chat.id)) {
         return msg.reply("Siz hali Botdan Royhadan o'tamansiz!")
     } else {
